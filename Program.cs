388c9eb34da6c2c3887812efc3a1f50b77b5ccc8
Prathamesh Rajput <rajputprathamesh1998@gmail.com>
2020-10-05 22:17:38 +0530

    [Prathamesh Rajput] Refractor. Completed Use Case 2

--- Program.cs
+++ Program.cs
@@ -14,12 +14,22 @@ namespace UserRegistration
         {
             //Use Case 1
             Console.Write("Enter Valid First Name: ");
-            string name=Console.ReadLine();
-            if (!Regex.IsMatch(name, "[A-Z][a-z]{3,}"))
+            string name = Console.ReadLine();
+            if (!Regex.IsMatch(name, "[A-Z][A-Za-z]{2,}"))
             {
-                Console.WriteLine("Name must start with Capital Letter and have atleast 3 letters");
+                Console.WriteLine("First Name must start with Capital Letter and have atleast 3 letters");
                 return false;
             }
+
+            // Use Case 2
+            Console.Write("Enter Valid Last Name: ");
+            string lname = Console.ReadLine();
+            if (!Regex.IsMatch(lname, "[A-Z][A-Za-z]{2,}"))
+            {
+                Console.WriteLine("Last Name must start with Capital Letter and have atleast 3 letters");
+                return false;
+            }
+
 
             return true;
         }
