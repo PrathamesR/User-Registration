762ece088367ece987cf726d7f77be61ccf871c9
Prathamesh Rajput <rajputprathamesh1998@gmail.com>
2020-10-13 00:57:42 +0530

    [Prathamesh Rajput] Add. Completed Use Case 12

--- Program.cs
+++ Program.cs
@@ -9,53 +9,54 @@ namespace UserRegistration
 {
     class Program
     {
+        public static string GetDetail(string pattern, string wxceptionMsg)
+        {
+            try
+            {
+                string val = Console.ReadLine();
+                if (Regex.IsMatch(val, pattern))
+                    return val;
+                else
+                    throw new UserDetailException(wxceptionMsg); 
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
 
         static bool RegisterUser()
         {
             //Use Case 1
             Console.Write("Enter Valid First Name: ");
-            string name = Console.ReadLine();
-            if (!Regex.IsMatch(name, "^[A-Z][A-Za-z]{2,}$"))
-            {
-                Console.WriteLine("First Name must start with Capital Letter and have atleast 3 letters");
+            string name = GetDetail("^[A-Z][A-Za-z]{2,}$", "First Name must start with Capital Letter and have atleast 3 letters");
+            if (name==null)
                 return false;
-            }
 
             // Use Case 2
             Console.Write("Enter Valid Last Name: ");
-            string lname = Console.ReadLine();
-            if (!Regex.IsMatch(lname, "^[A-Z][A-Za-z]{2,}$"))
-            {
-                Console.WriteLine("Last Name must start with Capital Letter and have atleast 3 letters");
+            string lname = GetDetail("^[A-Z][A-Za-z]{2,}$", "Last Name must start with Capital Letter and have atleast 3 letters");
+            if (lname == null)
                 return false;
-            }
 
             // Use Case 3
             Console.Write("Enter Valid Email Address: ");
-            string mail = Console.ReadLine();
-            if (!Regex.IsMatch(mail, @"^[A-Za-z]([A-Za-z0-9]+\.{0,1}[\-\+\*\!\_]*)*[a-zA-Z0-9]+@[a-z0-9]+(\.[a-z]{2,4})(\.[a-z]{2,4}){0,1}"))
-            {
-                Console.WriteLine("Invalid email format");
+            string mail = GetDetail(@"^[A-Za-z]([A-Za-z0-9]+\.{0,1}[\-\+\*\!_]*)*[a-zA-Z0-9]+@[a-z0-9]+(\.[a-z]{2,4})(\.[a-z]{2,4}){0,1}", "Invalid email format");
+            if (mail == null)
                 return false;
-            }
 
             //Use Case 4
             Console.Write("Enter Valid Phone Number: ");
-            string phoneNo = Console.ReadLine();
-            if (!Regex.IsMatch(phoneNo, "^\\+{0,1}[0-9]{2} [0-9]{10}$"))
-            {
-                Console.WriteLine("The number should be Country code follow by space and 10 digit number");
+            string phoneNo = GetDetail("^\\+{0,1}[0-9]{2} [0-9]{10}$", "The number should be Country code follow by space and 10 digit number");
+            if (phoneNo == null)
                 return false;
-            }
 
             //Use Case 8
             Console.Write("Enter Valid Password: ");
-            string pass = Console.ReadLine();
-            if (!Regex.IsMatch(pass, @"^(^(?=.{8,}$))(?=.*[A-Z])(?=.*[0-9])(^[a-zA-Z0-9]*[!@#$^&*()\-+=]{1}[a-zA-Z0-9]*$)"))
-            {
-                Console.WriteLine("The password should contain minimum 8 characters and a Capital Letter and exactly 1 special character");
+            string pass = GetDetail(@"^(^(?=.{8,}$))(?=.*[A-Z])(?=.*[0-9])(^[a-zA-Z0-9]*[!@#$^&*()\-+=]{1}[a-zA-Z0-9]*$)", "The password should contain minimum 8 characters and a Capital Letter and exactly 1 special character");
+            if (pass == null)
                 return false;
-            }
             
             Console.WriteLine("\nUser Details-\nName:"+name+" "+lname+" Mail: "+mail+" Password: "+pass+"\n");
             return true;
